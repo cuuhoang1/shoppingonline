405fa00c22ef26aa159d262758629d08fbb449c3
cuuhoang1 <vonguyencuuhoang.dt.2020@gmail.com>
2023-11-23 14:28:52 +0700

    shoppingonline

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.gn2ryig.mongodb.net',
+  DB_USER: 'cuuhoang1304',
+  DB_PASS: 'hoangvo123',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'cuuhoang1304',
+  JWT_EXPIRES: '36000000', // in milliseconds
+  EMAIL_USER: 'cuuhoang1304@gmail.com', // gmail service
+  EMAIL_PASS: '<ypny jrzw xrem plzd>'
+};
+module.exports = MyConstants;
